339d8f881d4f8b0d9664d43f084b4fb98013ef3e
Meghan Petiprin <meghanm99@gmail.com>
2017-09-14 22:04:45 -0700

    Added JavaScript and More
    Added JavaScript function on contact form
    Added nav bar
    Added more CSS and HTML5 updates

---
+++ script.js
@@ -0,0 +1,8 @@
+  function outputname() {
+var x,y;
+x=document.getElementById("contactinfo");
+y=x.elements["name"].value;
+document.write("Thanks for visiting my portfolio, "+y+" !!!!<br>");
+document.write("I'll be in touch soon!");
+}
+
